df3170e1f9567466b18561ef9cae52ff1929a39e
Eric Holk <eric.holk@gmail.com>
2014-04-07 19:41:48 -0400

    Working on adding Application. Naturally, things get more complicated, so I had to build some lemmas about related environments.

--- Stlc.v
+++ Stlc.v
@@ -8,6 +8,21 @@ Axiom sym_eq_dec: forall x y: Symbol, decidable (x = y).
 Axiom sym_beq: Symbol -> Symbol -> bool.
 
 Axiom sym_beq_eq: forall x y, (sym_beq x y = true) <-> x = y.
+Lemma sym_nbeq_neq: forall x y, (sym_beq x y = false) <-> x <> y.
+intros.
+split; intros.
+intro.
+case (sym_beq_eq x y); intros.
+apply H2 in H0.
+rewrite H0 in H.
+contradict H.
+discriminate.
+case (sym_beq_eq x y); intros.
+destruct (sym_beq x y).
+contradict H.
+auto.
+auto.
+Qed.
 
 Inductive Expr :=
 | Unit : Expr
@@ -91,7 +106,7 @@ Inductive steps_to_cont : StepCont -> Value -> Prop :=
 | step_refl : forall v, steps_to_cont (FinalK v) v
 | step_trans : forall s v, steps_to_cont (apply_k s) v -> steps_to_cont s v.
 
-Definition steps_to e v := steps_to_cont (step e (EmptyEnv Value) EmptyK) v.
+Definition steps_to e env v := steps_to_cont (step e env EmptyK) v.
 
 
 Inductive type_expr : (Env StlcType) -> Expr -> StlcType -> Prop :=
@@ -99,7 +114,10 @@ Inductive type_expr : (Env StlcType) -> Expr -> StlcType -> Prop :=
 | type_var : forall x t env, lookup x env = Some t -> type_expr env (Var x) t
 | type_lambda : forall env x b t' t,
   type_expr (ExtendEnv _ x t' env) b t
-  -> type_expr env (Lambda x b) (Fn t' t).
+  -> type_expr env (Lambda x b) (Fn t' t)
+| type_app : forall e1 e2 t1 t2 env,
+  type_expr env e1 (Fn t1 t2) -> type_expr env e2 t1
+  -> type_expr env (App e1 e2) t2.
 
 Inductive type_value : Value -> StlcType -> Prop :=
 | type_vunit : type_value VUnit TUnit
@@ -115,30 +133,128 @@ with related_env : (Env Value) -> (Env StlcType) -> Prop :=
   related_env (ExtendEnv _ x v env) (ExtendEnv _ x t tenv).
 
 Inductive type_cont : (StlcType) -> Cont -> StlcType -> Prop :=
-| type_empty_k : forall t, type_cont t EmptyK t.
+| type_empty_k : forall t, type_cont t EmptyK t
+| type_ratork : forall t2 t1 e2 env tenv k t',
+  related_env env tenv ->
+  type_expr tenv e2 t2 ->
+  type_cont t2 k t' ->
+  type_cont (Fn t2 t1) (RatorK e2 env k) t'
+| type_randk : forall t1 t2 t3 v k,
+  type_cont t2 k t3 ->
+  type_value v (Fn t1 t2) ->
+  type_cont t1 (RandK v k) t3.
 
 Inductive type_step : StepCont -> StlcType -> Prop :=
 | type_final : forall v t, type_value v t -> type_step (FinalK v) t
 | type_apply : forall t' k v t,
   type_value v t' -> type_cont t' k t -> type_step (ApplyK k v) t.
 
-Lemma preserve_step : forall t s s',
-  type_step s t -> s' = apply_k s -> type_step s' t.
-intros.
-inversion H; subst; intros.
-eauto using type_step.
-inversion H2; subst.
+Require Import Setoid.
+
+Lemma related_env_compat : forall env tenv,
+  related_env env tenv ->
+  forall x,
+    (forall v, lookup x env = Some v -> exists t, lookup x tenv = Some t)
+    /\
+    (forall t, lookup x tenv = Some t -> exists v, lookup x env = Some v).
+intros env tenv H; induction H; intros; split; intros.
+contradict H; discriminate.
+contradict H; discriminate.
+case (sym_eq_dec x x0); intros; subst.
+exists t; simpl.
+assert (sym_beq x0 x0 = true).
+apply sym_beq_eq; reflexivity.
+rewrite H2; reflexivity.
 simpl.
-eauto using type_step.
+assert (sym_beq x0 x = false).
+apply (sym_nbeq_neq x0 x).
+intro; apply H2; auto.
+rewrite H3.
+destruct (IHrelated_env x0).
+case (H4 v0); intros.
+simpl in H1.
+rewrite H3 in H1. exact H1.
+exists x1; auto.
+simpl.
+destruct (sym_eq_dec x0 x).
+rewrite H2.
+assert (sym_beq x x = true).
+apply (sym_beq_eq x x); auto.
+rewrite H3.
+exists v; auto.
+assert (sym_beq x0 x = false).
+apply (sym_nbeq_neq x0 x); auto.
+rewrite H3.
+destruct (IHrelated_env x0).
+apply (H5 t0).
+simpl in H1.
+rewrite H3 in H1.
+exact H1.
 Qed.
 
-Lemma preserve_first : forall e t s,
+Lemma related_env_equiv : forall env tenv,
+  related_env env tenv ->
+  forall x v t,
+    lookup x env = Some v ->
+    lookup x tenv = Some t ->
+    type_value v t.
+intros env tenv H;
+induction H; intros.
+contradict H0; simpl; discriminate.
+destruct (sym_eq_dec x0 x); subst.
+simpl in H1.
+assert (sym_beq x x = true).
+apply  (sym_beq_eq x x).
+reflexivity.
+rewrite H3 in H1.
+injection H1; intros; subst.
+simpl in H2.
+rewrite H3 in H2; injection H2; intros; subst.
+exact H0.
+simpl in H1; simpl in H2.
+assert (sym_beq x0 x = false).
+apply (sym_nbeq_neq x0 x).
+exact H3.
+rewrite H4 in H1; rewrite H4 in H2.
+apply (IHrelated_env x0).
+exact H1.
+exact H2.
+Qed.
+
+Lemma preserve_eval : forall e env tenv k t t',
+  related_env env tenv ->
+  type_cont t' k t ->
+  type_expr tenv e t' ->
+  type_step (step e env k) t.
+intros.
+induction H1.
+simpl.
+apply (type_apply TUnit).
+apply type_vunit.
+auto.
+simpl.
+case (related_env_compat env env0 H x); intros.
+case (H3 t0); auto; intros.
+rewrite H4.
+apply (type_apply t0).
+
+Lemma preserve_step : forall t s,
+  type_step s t -> type_step (apply_k s) t.
+intros.
+induction H.
+eauto using type_step.
+simpl.
+destruct k.
+inversion H0; subst.
+eauto using type_step.
+
+Lemma preserve_first : forall e t,
   type_expr (EmptyEnv _) e t 
-  -> step e (EmptyEnv _) EmptyK = s
-  -> type_step s t.
+  -> type_step (step e (EmptyEnv _) EmptyK) t.
 intros.
 inversion H; subst.
-simpl.
+unfold step.
+
 apply (type_apply TUnit).
 eauto using type_value.
 eauto using type_cont.
@@ -152,6 +268,13 @@ auto using related_env.
 inversion H; subst.
 apply H3.
 eauto using type_cont.
+inversion H1; subst.
+contradict H0; discriminate.
+compute.
+apply (type_apply (Fn t1 t)).
+apply (type_closure _ _ _ (EmptyEnv StlcType)).
+auto using related_env.
+auto.
 Qed.
 
 Lemma preserve_steps_to_cont : forall t s v,
