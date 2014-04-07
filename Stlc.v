47c72e7546d395a995d9f23ec98c0232e1510fa6
Eric Holk <eric.holk@gmail.com>
2014-04-07 15:43:09 -0400

    Expanded preservation lemma.

--- Stlc.v
+++ Stlc.v
@@ -104,14 +104,33 @@ Inductive type_cont : (StlcType) -> Cont -> StlcType -> Prop :=
 | type_empty_k : forall t, type_cont t EmptyK t.
 
 Inductive type_step : StepCont -> StlcType -> Prop :=
-| type_final : forall v t, type_value v t -> type_step (FinalK v) t.
+| type_final : forall v t, type_value v t -> type_step (FinalK v) t
+| type_apply : forall t' k v t,
+  type_value v t' -> type_cont t' k t -> type_step (ApplyK k v) t.
 
 Lemma preserve_step : forall t s s',
   type_step s t -> s' = apply_k s -> type_step s' t.
 intros.
 induction H.
 simpl in H0; subst.
-auto using type_step.
+eauto using type_step.
+simpl in H0.
+inversion H; subst.
+inversion H1; subst.
+eauto using type_step.
+Qed.
+
+Lemma preserve_first : forall e t s,
+  type_expr (EmptyEnv _)  e t 
+  -> step e (EmptyEnv _) EmptyK = s
+  -> type_step s t.
+intros.
+induction H.
+compute in H0.
+subst.
+apply (type_apply TUnit).
+auto using type_value.
+auto using type_cont.
 Qed.
 
 Theorem type_safe : forall e t v,
